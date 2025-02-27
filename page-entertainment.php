d5bc30ddc34ef0bc89442632306564f2dcf54003
Redon <Redon.Bytyqi@student.cacttus.education>
2025-02-27 11:18:25 -0800

    .

--- page-entertainment.php
+++ page-entertainment.php
@@ -17,6 +17,7 @@ if ($query->have_posts()) :
 ?>
     <div class="news-item">
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <img src="<?php the_post_thumbnail(); ?>" alt="">
         <p><?php the_excerpt(); ?></p>
     </div>
 <?php
