fbbf416fd7b92d097f425ffc223031329c74de7f
Redon <Redon.Bytyqi@student.cacttus.education>
2025-03-01 15:23:55 +0100

    .

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
-       
-
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
